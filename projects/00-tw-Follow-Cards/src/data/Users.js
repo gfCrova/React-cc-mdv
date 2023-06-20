04182a0675229fc36b95e79268482743fcf004ea
gfCrova <giancrova.cl5@gmail.com>
2023-06-19 21:02:40 -0300

    refactorización creando folder data and move users

---
+++ projects/00-tw-Follow-Cards/src/data/Users.js
@@ -0,0 +1,23 @@
+
+export const users = [
+    {
+        userName: "elonmusk",
+        name: "Elon Musk",
+        isFollowing: false
+    },
+    {
+        userName: "midudev",
+        name: "Miguel Ángel Durán",
+        isFollowing: true
+    },
+    {
+        userName: "giancrova",
+        name: "Gian Crova",
+        isFollowing: false
+    },
+    {
+        userName: "stevejobbs",
+        name: "Steve Jobs",
+        isFollowing: true
+    }
+];
